0a3b8304b894b0bb87e93a8f2c091659136c229e
Ryan Halik <ryan.okuinghttons@gmail.com>
2025-11-05 22:23:27 -0800

    add test cases to mk.ps1

--- Assignment_2/mk.ps1
+++ Assignment_2/mk.ps1
@@ -3,5 +3,19 @@
 g++ assignment_2.cpp -o assignment_2
 
 #generic run statement
-# ./assignment_2.exe route_summary .\data\04_June_Route1.txt
+./assignment_2.exe route_summary .\data\04_June_Route1.txt
 ./assignment_2 days ./data/04_June_Route1.txt
+./assignment_2 days ./data/05_July_Route1.txt
+./assignment_2 days ./data/06_August_Route1.txt
+./assignment_2 days ./data/07_June_AllRoutes.txt
+./assignment_2 days ./data/08_JanFebMar_Route1.txt
+./assignment_2 days ./data/09_JuneJulyAug_AllRoutes.txt
+./assignment_2 days ./data/10_Sept2017_Aug2022_AllRoutes.txt
+./assignment_2 days ./data/11_ShortLineI.txt
+./assignment_2 days ./data/14_EmptyFieldI.txt
+./assignment_2 days ./data/15_EmptyFieldII.txt
+./assignment_2 days ./data/16_EmptyFieldIII.txt
+./assignment_2 days ./data/17_NonNumericI.txt
+./assignment_2 days ./data/18_NonNumericII.txt
+./assignment_2 days ./data/19_BadTimeI.txt
+./assignment_2 days ./data/20_BadTimeII.txt
